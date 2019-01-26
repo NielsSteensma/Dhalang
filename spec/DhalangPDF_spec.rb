ebbcd84a36893efed49d41e0b36d1920093c5997
Niels Steensma <nielssteensma@yahoo.nl>
2019-01-26 14:07:59 +0100

    Made the pdf generator return a string with binary pdf content instead of tempfiles

--- spec/DhalangPDF_spec.rb
+++ spec/DhalangPDF_spec.rb
@@ -15,18 +15,18 @@ describe '#get_from_url' do
       expect { DhalangPDF.get_from_url("https://www.google.com") }.to_not raise_error(URI::InvalidURIError)
     end
 
-    it 'should return an object of type TempFile' do
-      expect(DhalangPDF.get_from_url("https://www.google.com")).to be_an_instance_of(Tempfile)
+    it 'should return an object of type string' do
+      expect(DhalangPDF.get_from_url("https://www.google.com")).to be_an_instance_of(String)
     end
 
     it 'should return a file that is not empty' do
-      pdf = DhalangPDF.get_from_url("https://www.google.com")
-      expect(File.zero?(pdf.path)).to be false
+      pdf_binary_content = DhalangPDF.get_from_url("https://www.google.com")
+      expect(File.zero?(create_pdf_file(pdf_binary_content))).to be false
     end
 
     it 'should return pdf containing the webpage' do
-      pdf = DhalangPDF.get_from_url("https://www.google.com")
-      pdf_reader = PDF::Reader.new(pdf.path)
+      pdf_binary_content = DhalangPDF.get_from_url("https://www.google.com")
+      pdf_reader = PDF::Reader.new(create_pdf_file(pdf_binary_content).path)
       expect(pdf_reader.page(1).to_s.include?("Google")).to be true
     end
   end
@@ -37,8 +37,8 @@ describe '#get_from_html' do
   context 'invalid html' do
     it 'should return empty pdf' do
       html = ""
-      pdf = DhalangPDF.get_from_html(html)
-      pdf_reader = PDF::Reader.new(pdf.path)
+      pdf_binary_content = DhalangPDF.get_from_html(html)
+      pdf_reader = PDF::Reader.new(create_pdf_file(pdf_binary_content).path)
       expect(pdf_reader.page(1).to_s.empty?).to be true
     end
   end
@@ -46,9 +46,17 @@ describe '#get_from_html' do
   context 'valid html' do
     it 'should return pdf containing the html' do
       html = "<html><head></head><body><h1>hello world</h1></body></html>"
-      pdf = DhalangPDF.get_from_html(html)
-      pdf_reader = PDF::Reader.new(pdf.path)
+      pdf_binary_content = DhalangPDF.get_from_html(html)
+      pdf_reader = PDF::Reader.new(create_pdf_file(pdf_binary_content).path)
       expect(pdf_reader.page(1).to_s.include?("hello world")).to be true
     end
   end
+end
+
+def create_pdf_file(binary_pdf_content)
+  pdf_file = Tempfile.new('pdf')
+  pdf_file.binmode
+  pdf_file.write(binary_pdf_content)
+  pdf_file.rewind
+  return pdf_file
 end
